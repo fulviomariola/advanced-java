dd4abc4d130ea23d37b77572bdebd08a1c3d46e4
Fulvio Mariola <fulvio.mariola@gmail.com>
2022-11-22 12:25:44 +0100

    changed hello mmessage

--- Simple/src/app/App.java
+++ Simple/src/app/App.java
@@ -1,8 +1,8 @@
+
 package app;
 
 public class App {
-
     public static void main(String[] args) {
-        System.out.println("test another times");
+        System.out.println("Hello world!!!");
     }
 }
