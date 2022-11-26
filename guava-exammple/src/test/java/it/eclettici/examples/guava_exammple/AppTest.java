f01de3ef4f0d421c37c9ab533705c463ca75aa67
Fulvio Mariola <fulvio.mariola@gmail.com>
2022-11-26 17:42:00 +0100

    Addes Guava

---
+++ guava-exammple/src/test/java/it/eclettici/examples/guava_exammple/AppTest.java
@@ -0,0 +1,20 @@
+package it.eclettici.examples.guava_exammple;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
