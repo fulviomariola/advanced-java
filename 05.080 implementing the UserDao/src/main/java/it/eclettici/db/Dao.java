9f91def4667f272e763ea76dbf057aca6ffb5541
Fulvio Mariola <fulvio.mariola@gmail.com>
2022-12-10 00:28:26 +0100

    UserDao

--- 05.080 implementing the UserDao/src/main/java/it/eclettici/db/Dao.java
+++ 05.080 implementing the UserDao/src/main/java/it/eclettici/db/Dao.java
@@ -1,10 +1,12 @@
 package it.eclettici.db;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface Dao<T> {
 	void save(T t);
 	Optional<T> findById(int id);
 	void update(T t);
-	void delete(T t);
+	void delete(T t); 
+	List<T> getall();
 }
---
+++ 05.080 implementing the UserDao/src/main/java/it/eclettici/db/UserDaoImpl.java
@@ -0,0 +1,38 @@
+package it.eclettici.db;
+
+import java.util.List;
+import java.util.Optional;
+
+public class UserDaoImpl implements UserDao {
+
+	@Override
+	public void save(User t) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Optional<User> findById(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void update(User t) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void delete(User t) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<User> getall() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
