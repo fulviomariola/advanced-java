bc96325481b0f0299ffcd1a8bd7fb082576df5f6
Fulvio Mariola <fulvio.mariola@gmail.com>
2022-12-09 19:00:21 +0100

    Dao interface User Class

---
+++ 05.060 User Class/src/main/java/it/eclettici/db/App.java
@@ -0,0 +1,30 @@
+package it.eclettici.db;
+
+import java.sql.SQLException;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		
+		var db = Database.instance();
+		
+		try {
+			db.connect();
+			System.out.println("Sei Connesso al database!!!");
+		} catch (SQLException e) {
+			System.out.println("Connessione al database non riuscita");
+		}
+		
+		
+		
+		try {
+			db.disconnect();
+		} catch (SQLException e) {
+			System.out.println("Disconnesso dal database");
+		}
+	}
+}
