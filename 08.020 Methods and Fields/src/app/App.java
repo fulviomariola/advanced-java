b2debf633d2e473db2b027e2697bd39ef2498366
Fulvio Mariola <fulvio.mariola@gmail.com>
2022-12-24 10:52:46 +0100

    Methods and Fields

---
+++ 08.020 Methods and Fields/src/app/App.java
@@ -0,0 +1,25 @@
+package app;
+
+import java.util.Arrays;
+
+class User {
+	public int id;
+}
+
+class Employee extends User {
+	public String name;
+	
+	private String passwd;
+}
+
+public class App {
+	public static void main(String[] args) {
+		
+		Class<Employee> clazz = Employee.class;
+		
+		System.out.println(Arrays.asList(clazz.getFields()));
+		
+		System.out.println(Arrays.asList(clazz.getDeclaredFields()));
+		
+	}
+}
