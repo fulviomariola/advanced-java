deeac547852d4656096af8b32df505c68bdca578
Fulvio Mariola <fulvio.mariola@gmail.com>
2022-12-29 09:50:33 +0100

    Annotation Use Case Scenario

---
+++ 09.010 Annotation Use Case Scenario/src/app/App.java
@@ -0,0 +1,12 @@
+package app;
+
+public class App {
+	public static void main(String[] args) {
+		User user = new User(0L, "Thor");
+		
+		var rep = new Repository<User>();
+		
+		rep.save(user);
+		//System.out.println(rep);
+	}
+}
