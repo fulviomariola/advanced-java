164c2961f40e6a53543a0a761a7abba9f1e29c18
Fulvio Mariola <fulvio.mariola@gmail.com>
2022-12-30 10:04:10 +0100

    Implementing Annotation Code

---
+++ 09.030 Implementing Annotation Code/src/app/User.java
@@ -0,0 +1,41 @@
+package app;
+
+public class User {
+	
+	@Field
+	@Deprecated
+	private Long id;
+	
+	@Field
+	private String name;
+	
+	@Field
+	private int sequnce;
+
+	public User(Long id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", name=" + name + "]";
+	}
+
+}
