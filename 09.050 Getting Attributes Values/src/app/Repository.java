498e4eccedd1cf74a9a7e5ab99c104c6ba8e81f5
Fulvio Mariola <fulvio.mariola@gmail.com>
2022-12-30 10:57:09 +0100

    Getting Attributes Values

---
+++ 09.050 Getting Attributes Values/src/app/Repository.java
@@ -0,0 +1,30 @@
+package app;
+
+public class Repository<T> {
+	public void save(T t) {
+		
+		var clazz = t.getClass();
+		
+		var fields = clazz.getDeclaredFields();
+		
+		for(var field: fields) {
+			var annotations = field.getAnnotationsByType(Field.class);
+			
+			// se è 0 significa che non ci sono annotations
+			if(annotations.length == 0) {
+				continue;
+			}
+			
+			var annotation = annotations[0];
+			
+			var fieldName = annotation.value();
+			var isKey = annotation.isKey();
+			
+			if(fieldName.length() == 0) {
+				fieldName = field.getName();
+			}
+			
+			System.out.println(fieldName +"-"+ isKey);
+		}
+	}
+}
