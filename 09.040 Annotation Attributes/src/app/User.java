1b70630551e9c330b6343fe2924e9ffbd547e5a6
Fulvio Mariola <fulvio.mariola@gmail.com>
2022-12-30 10:39:32 +0100

    Annotation Attributes

---
+++ 09.040 Annotation Attributes/src/app/User.java
@@ -0,0 +1,39 @@
+package app;
+
+public class User {
+	
+	@Field(value="id", isKey=true)
+	private Long id;
+	
+	@Field
+	private String name;
+	
+	private int sequnce;
+
+	public User(Long id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", name=" + name + "]";
+	}
+
+}
