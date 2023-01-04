c1a203bc8b1b55914e026fa70a7a0d7b8a80541a
Fulvio Mariola <fulvio.mariola@gmail.com>
2023-01-04 09:23:51 +0100

    Class Annotations

---
+++ 09.060 Class Annotations/src/app/User.java
@@ -0,0 +1,40 @@
+package app;
+
+@Entity("useryyy")
+public class User {
+	
+	@Field(columnName="id", isKey=true)
+	private Long id;
+	
+	@Field
+	private String name;
+	
+	private int sequnce;
+
+	public User(Long id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+//	@Override
+//	public String toString() {
+//		return "User [id=" + id + ", name=" + name + "]";
+//	}
+
+}
