e3d1185f3365312597ca191c4ab5b95c2ebe1cf0
Fulvio Mariola <fulvio.mariola@gmail.com>
2023-01-11 11:58:04 +0100

    Adding a Menu

---
+++ 10.050 Adding a Menu/src/main/java/gui/MainPanel.java
@@ -0,0 +1,13 @@
+package gui;
+
+import javax.swing.JPanel;
+import java.awt.Color;
+
+public class MainPanel extends JPanel {
+	private static final long serialVersionUID = 1L;
+	 
+	public MainPanel() {
+		setBackground(Color.GREEN);
+	}
+}
+ 
