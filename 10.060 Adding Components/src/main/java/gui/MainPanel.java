9651c9a5a2b0b4fa2d43e5d8b4ab2059744e303e
Fulvio Mariola <fulvio.mariola@gmail.com>
2023-01-12 20:37:44 +0100

    Adding Components

--- 10.060 Adding Components/src/main/java/gui/MainPanel.java
+++ 10.060 Adding Components/src/main/java/gui/MainPanel.java
@@ -1,13 +1,31 @@
 package gui;
 
 import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+
+import java.awt.BorderLayout;
 import java.awt.Color;
 
 public class MainPanel extends JPanel {
 	private static final long serialVersionUID = 1L;
 	 
-	public MainPanel() {
-		setBackground(Color.GREEN);
+	public MainPanel() {		
+		var formLabel = new JLabel("Aggiungi Utente");
+		var nameLabel = new JLabel("Utente:"); 
+		var namePassword = new JLabel("Password:");
+		
+		var nameField = new JTextField();
+		var passField = new JTextField();
+		
+		var addButton = new JButton("Save");
+		
+		setLayout(new BorderLayout());
+		
+		add(formLabel, BorderLayout.NORTH);
+		add(nameLabel, BorderLayout.CENTER);
+		//add(namePassword, BorderLayout.);
 	}
 }
  
