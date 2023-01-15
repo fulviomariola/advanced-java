d971a13d53f12804c3cb115b6b69257c167d7402
Fulvio Mariola <fulvio.mariola@gmail.com>
2023-01-15 12:58:20 +0100

    Weigths and Anchors

---
+++ 10.090 Weigths and Anchors/src/main/java/gui/MainPanel.java
@@ -0,0 +1,63 @@
+package gui;
+
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+
+public class MainPanel extends JPanel {
+	private static final long serialVersionUID = 1L;
+	 
+	public MainPanel() {		
+		var formLabel = new JLabel("Aggiungi Utente");
+		var nameLabel = new JLabel("Utente:"); 
+		var passLabel = new JLabel("Password:");
+		
+		var nameField = new JTextField(15);
+		var passField = new JTextField(15);
+		
+		var addButton = new JButton("Save");
+		
+		setLayout(new GridBagLayout());
+		
+		var gc = new GridBagConstraints();
+		
+		gc.gridx = 0;
+		gc.gridy = 0;
+		
+		gc.weightx = 1;
+		
+		gc.gridwidth = 2;
+		add(formLabel, gc);
+		
+		gc.gridy++;
+		gc.gridwidth = 1;
+		
+		gc.gridx = 0;
+		gc.anchor = GridBagConstraints.LINE_END;
+		add(nameLabel, gc);
+		
+		gc.gridx = 1;
+		gc.anchor = GridBagConstraints.LINE_START;
+		add(nameField, gc);
+		
+		gc.gridy++;
+		
+		gc.gridx = 0;
+		gc.anchor = GridBagConstraints.LINE_END;
+		add(passLabel, gc);
+		
+		gc.gridx = 1;
+		gc.anchor = GridBagConstraints.LINE_START;
+		add(passField, gc);
+		
+		gc.gridy++;
+		gc.gridx = 1;
+		
+		add(addButton, gc);
+	}
+}
+ 
