28f7f60755b3038a31c759086445206e5bbe1bbc
Fulvio Mariola <fulvio.mariola@gmail.com>
2023-01-30 19:33:33 +0100

    Dao interface

--- 05.050 Dao interface/src/main/java/it/eclettici/db/App.java
+++ 05.050 Dao interface/src/main/java/it/eclettici/db/App.java
@@ -8,7 +8,7 @@ import java.sql.SQLException;
  */
 public class App {
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		System.out.println("Hello World");
 		
 		var db = Database.instance();
 		
