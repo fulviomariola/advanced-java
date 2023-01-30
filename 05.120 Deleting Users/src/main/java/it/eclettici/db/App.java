6327c2d71e4b0c817fcda8e1fed8fba09af65dce
Fulvio Mariola <fulvio.mariola@gmail.com>
2023-01-30 19:39:56 +0100

    Deleting Users

--- 05.120 Deleting Users/src/main/java/it/eclettici/db/App.java
+++ 05.120 Deleting Users/src/main/java/it/eclettici/db/App.java
@@ -8,7 +8,7 @@ import java.sql.SQLException;
  */
 public class App {
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		System.out.println("Hello World");
 		
 		// Connessione al DB
 		var db = Database.instance();
