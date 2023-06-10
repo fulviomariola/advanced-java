50a6f3919c016b4726013d5e8f4779e9c856fde0
Fulvio Mariola <fulvio.mariola@gmail.com>
2023-06-10 13:37:45 +0200

    11.160_JSON_Serialization

---
+++ 11.160_JSON_Serialization/app/src/main/resources/web/UserServlet.java
@@ -0,0 +1,59 @@
+package web;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.gson.Gson;
+
+/**
+ * Servlet implementation class UserServlet
+ */
+@WebServlet("/users")
+public class UserServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see Htt pServlet#HttpServlet()
+     */
+    public UserServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		List<User> users = List.of(new User("Asterix","passwd"), new User("Obelix", "passwd2"));
+		
+		Gson gson = new Gson();
+		
+		String json = gson.toJson(users);
+		System.out.println(json);
+		
+		response.setContentType("application/json");
+		
+		response.getWriter().println(json);
+	} 
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		var name = request.getParameter("name");
+		var passwd = request.getParameter("password");
+		
+		response.setContentType("text/html");
+		response.getWriter().println("<html> Form submitted: " + name + "</html>");
+		
+		System.out.println(name + ": " + passwd);
+	}
+
+}
