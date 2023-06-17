4cc7ef2b01bdd453e4ca81434b9cddde8ac67deb
Fulvio Mariola <fulvio.mariola@gmail.com>
2023-06-17 15:29:08 +0200

    12.060_Configuration

---
+++ 12.060_Configuration/app/src/main/java/main/Runner.java
@@ -0,0 +1,25 @@
+package main;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Component;
+
+import greeters.Greeter;
+
+@Component
+public class Runner implements CommandLineRunner{
+
+	@Autowired
+	private Greeter greeter;
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		greeter.greet();
+				
+	}
+		
+	
+}
