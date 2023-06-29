56af90410e32a3abb378241e226cb7cd4b4ba928
Fulvio Mariola <fulvio.mariola@gmail.com>
2023-06-29 19:07:31 +0200

    13.040-Rest-Controllers

---
+++ 13.040-Rest-Controllers/src/main/java/app/entities/Book.java
@@ -0,0 +1,84 @@
+package app.entities;
+
+public class Book {
+	private Long id;
+	private String title;
+	private String other;
+	
+	public Book() {
+		
+	}
+
+	public Book(String title, String other) {
+		this.title = title;
+		this.other = other;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getOther() {
+		return other;
+	}
+
+	public void setOther(String other) {
+		this.other = other;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((other == null) ? 0 : other.hashCode());
+		result = prime * result + ((title == null) ? 0 : title.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Book other = (Book) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (this.other == null) {
+			if (other.other != null)
+				return false;
+		} else if (!this.other.equals(other.other))
+			return false;
+		if (title == null) {
+			if (other.title != null)
+				return false;
+		} else if (!title.equals(other.title))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Book [id=" + id + ", title=" + title + ", other=" + other + "]";
+	}
+	
+	
+}
