61d3aaf0b6ac7c485ab1c2570b1d20139767bd59
Fulvio Mariola <fulvio.mariola@gmail.com>
2023-07-06 14:21:23 +0200

    update project

---
+++ 13.040-Rest-Controllers/src/main/java/app/controllers/BookController.java
@@ -0,0 +1,30 @@
+package app.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+//import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import app.entities.Book;
+
+@RestController
+public class BookController {
+	
+	@GetMapping("/books")
+	public Book books() {
+		
+		var book = new Book("Charles", "Great Expectations");
+		
+		return book;
+	}
+	
+	@PostMapping("/books")
+	public Book createBook(@RequestBody Book book) {
+		
+		System.out.println("Creating: " + book);
+		book.setId(7L);
+		return book;
+	}
+	
+}
